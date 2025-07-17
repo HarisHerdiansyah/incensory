eea5a973bdbe38aa0ba33f8e49d39fc6b0f3b51f
HarisHerdiansyah <haris54237@gmail.com>
2025-07-17 17:20:31 +0700

    fix linter column

--- components/cms/products/ProductColumn.tsx
+++ components/cms/products/ProductColumn.tsx
@@ -1,14 +1,9 @@
 'use client';
 
-import Link from 'next/link';
-import { useState } from 'react';
-import { toast } from 'react-toastify';
-import clsx from 'clsx';
 import { ColumnDef } from '@tanstack/react-table';
-import { archiveProduct } from '@/actions/products';
-import { Button } from '@/components/ui/button';
 import { ProductResponse } from './types';
-import Loader from '@/components/Loader';
+import ProductActionBtn from './ProductActionBtn';
+
 
 const columns: ColumnDef<ProductResponse>[] = [
   {
@@ -29,47 +24,7 @@ const columns: ColumnDef<ProductResponse>[] = [
   },
   {
     header: 'Aksi',
-    cell: ({ row }) => {
-      const [isLoading, setIsLoading] = useState(false);
-      const product = row.original;
-
-      const handleArchive = async () => {
-        setIsLoading(true);
-        const response = await archiveProduct(product.id, !product.isVisible);
-        if (response.success) {
-          toast.success(response.message);
-        } else {
-          toast.error(response.message);
-        }
-        setIsLoading(false);
-      };
-
-      return (
-        <>
-          {isLoading && <Loader />}
-          <div className='flex gap-2'>
-            <Link href={`/cms/products/${product.id}/edit`}>
-              <Button
-                className='text-black bg-amber-400 hover:bg-amber-500 cursor-pointer'
-                size='sm'
-              >
-                Edit
-              </Button>
-            </Link>
-            <Button
-              className={clsx('cursor-pointer', {
-                'bg-red-600 hover:bg-red-700': product.isVisible,
-                'bg-accent': !product.isVisible,
-              })}
-              size='sm'
-              onClick={handleArchive}
-            >
-              {product.isVisible ? 'Arsipkan' : 'Tampilkan'}
-            </Button>
-          </div>
-        </>
-      );
-    },
+    cell: ({ row }) => <ProductActionBtn row={row} />,
   },
 ];
 
